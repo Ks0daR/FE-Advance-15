6105fceea1feb39f563e0e8ccec94d95a1ad6eb2
Ivan Shevchenko <rayserviz@gmail.com>
2019-11-02 20:30:00 +0200

    hw07 fix bugs

--- homeworks/hw_07/js/task_05.js
+++ homeworks/hw_07/js/task_05.js
@@ -2,10 +2,12 @@ const refs = {
   input: document.querySelector('input[type="text"]'),
   output: document.querySelector('#name-output'),
 };
-
+const def = refs.output.textContent;
 function changeName(x) {
   const { target } = x;
-  refs.output.textContent = target.value;
+  if (target.textLength !== 0) {
+    refs.output.textContent = target.value;
+  } else refs.output.textContent = def;
 }
 
 refs.input.addEventListener('input', changeName);
