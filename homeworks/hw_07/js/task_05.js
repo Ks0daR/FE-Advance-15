346ffffdb68024e3c0772fac0ba1cdf3cb274f93
Ivan Shevchenko <rayserviz@gmail.com>
2019-11-03 12:51:02 +0200

    fix task_05

--- homeworks/hw_07/js/task_05.js
+++ homeworks/hw_07/js/task_05.js
@@ -5,9 +5,10 @@ const refs = {
 const def = refs.output.textContent;
 function changeName(x) {
   const { target } = x;
-  if (target.textLength !== 0) {
-    refs.output.textContent = target.value;
-  } else refs.output.textContent = def;
+  console.log(x);
+  if (target.value === '') {
+    refs.output.textContent = def;
+  } else refs.output.textContent = target.value;
 }
 
 refs.input.addEventListener('input', changeName);
